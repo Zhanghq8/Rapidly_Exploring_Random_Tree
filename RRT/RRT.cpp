0ad3e8b200da29d5730f9b5f1ebb86e640225a25
Zhanghq8 <hzhang8@wpi.edu>
2019-06-20 15:19:19 -0400

    without obstacle

--- RRT/RRT.cpp
+++ RRT/RRT.cpp
@@ -3,14 +3,19 @@
 
 RRT::Vertex::Vertex(Vec2i coordinates_, Vertex *parent_)
 {
-	parent = parent_;
 	coordinates = coordinates_;
+	parent = parent_;
 }
 
 void RRT::RRT::setmap(float map_width_, float map_height_)
 {
 	map_width = map_width_;
 	map_height = map_height_;
+}
+
+void RRT::RRT::setgoalbias(float goal_bias_)
+{
+	goal_bias = goal_bias_;
 }
 
 void RRT::RRT::setstepsize(float step_size_)
@@ -58,15 +63,22 @@ float RRT::RRT::euclidean_dis(Vec2i source_, Vec2i goal_) // calculate the eucli
 	return e_distance; 
 }
 
-RRT::Vec2i RRT::RRT::GenerateRandomPoint()
+RRT::Vec2i RRT::RRT::GenerateRandomPoint(Vec2i goal_)
 {
 	Vec2i randompoint;
 	std::random_device rd;
 	std::mt19937 gen(rd());
-	std::uniform_real_distribution<> x(-randompoint_size, randompoint_size);
-	std::uniform_real_distribution<> y(-randompoint_size, randompoint_size);
+	std::uniform_real_distribution<> x(0, randompoint_size);
+	std::uniform_real_distribution<> y(0, randompoint_size);
+
 	randompoint.x = x(gen);
 	randompoint.y = y(gen);
+
+	bool setgoal = (rand() % 100 + 1) <= 7;
+	if (setgoal == true )
+	{
+		return goal_;
+	}
 	return randompoint;
 }
 
@@ -78,7 +90,6 @@ RRT::Vertex* RRT::RRT::getClosestVertex(std::set<Vertex*>& Vertices_, Vec2i rand
 		if (euclidean_dis(vertex->coordinates, randompoint_) < min_distance) {
 			min_distance = euclidean_dis(vertex->coordinates, randompoint_);
 			closestvertex = vertex;
-
 		}
 	}
 	return closestvertex;
@@ -91,11 +102,11 @@ bool RRT::RRT::movetorandom(Vertex* closestvertex_, Vec2i randompoint_)
 	Vec2i vertextemp;
 	vertextemp.x = closestvertex_->coordinates.x + step_size * cos(theta);
 	vertextemp.y = closestvertex_->coordinates.y + step_size * sin(theta);
+	std::cout << "vertextemp: " << vertextemp.x << " " << vertextemp.y << std::endl;
 	if (isValid(vertextemp, closestvertex_->coordinates) == true) 
 	{	
 		Vertex* newvertex = new Vertex(vertextemp, closestvertex_);
-		closestvertex_->parent = newvertex;
-		lastvertex = newvertex;
+		current = newvertex;
 		VertexSet.insert(newvertex);
 		return true;
 	}
@@ -107,50 +118,49 @@ void RRT::RRT::findPath(Vec2i source_, Vec2i goal_)
 	lastvertex = NULL;
 	bool done_flag = false;
 	VertexSet.insert(new Vertex(source_));
-	Vertex* root = *VertexSet.begin();
+	current = *VertexSet.begin();
+	// std::cout << "path root " << current->parent << std::endl;
 	int current_iterations = 0;
-	while (done_flag != true && current_iterations < max_iterations) 
+	while (done_flag != true && current_iterations <= max_iterations) 
 	{	
-		// std::cout << current_iterations << std::endl;
-		
-		Vec2i randompoint = GenerateRandomPoint();
-		Vertex* closestvertex = getClosestVertex(VertexSet, goal_);
-		if (movetorandom(closestvertex, randompoint) == true)
+		std::cout << current_iterations << std::endl;
+		Vec2i randompoint = GenerateRandomPoint(goal_);
+		// std::cout << "random point " << randompoint.x << std::endl;
+		Vertex* closestv= getClosestVertex(VertexSet, randompoint);
+		std::cout << "MOVE" << movetorandom(closestv, randompoint) << std::endl;
+		if (movetorandom(closestv, randompoint) == true)
 		{
 			current_iterations++;
-			if (isGoal(lastvertex->coordinates, goal_) == true)
+			if (isGoal(current->coordinates, goal_) == true)
 			{
 				done_flag = true;
+				Vertex* goalvertex = new Vertex(goal_, current);
+				current = goalvertex;
 				std::cout << "Found a path." << std::endl;
-
 			}
 		}
 		if (current_iterations == max_iterations)
 		{
 			std::cout << "No path found." << std::endl;
-			root = NULL;
-		}
-		if (current_iterations < 100)
-		{
-			std::cout << lastvertex->coordinates.x << " " << lastvertex->coordinates.y << std::endl;
 		}
 	}
-	releaseVertices(VertexSet);
-	while (root != NULL) 
+	
+	while (current != NULL) 
 	{
-		path.push_back(root->coordinates);
-		root = root->parent;
+		path.push_back(current->coordinates);
+		current = current->parent;
 	}
 	reverse(path.begin(), path.end());
 	if (!path.empty()) 
 	{
+		std::cout << "path size" <<  path.size() << std::endl;
 		for (auto ele:path)
 		{
 			std::cout << "[" << ele.x << "," << ele.y << "] "; 
 		}
 		std::cout << "\n";
 	}
-	
+	releaseVertices(VertexSet);
 }
 
 void RRT::RRT::releaseVertices(std::set<Vertex*>& Vertices_)
@@ -167,13 +177,14 @@ int main()
 	RRT::RRT temp;
 	temp.setmap(30.0, 30.0);
 	temp.setstepsize(3.0);
+	temp.setgoalbias(0.07);
 	temp.setrandompointsize(5.0);
 	temp.setgoalradius(1.0);
 	temp.setmaxiterations(10000);
 	RRT::Vec2i start, goal;
 	start.x = 10.0;
 	start.y = 10.0;
-	goal.x = 20.0;
-	goal.y = 20.0;
+	goal.x = 25.0;
+	goal.y = 25.0;
 	temp.findPath(start, goal);
 }
--- RRT/RRT.h
+++ RRT/RRT.h
@@ -32,21 +32,24 @@ namespace RRT
 		float step_size; //generate new point step_size from closest vertex towards random point
 		int delta;		
 		int max_iterations;
+		float goal_bias;
 		float goal_radius; 
 		Vertex* lastvertex;
+		Vertex* current;
 		std::vector<Vec2i> path;
 		std::set<Vertex*> VertexSet;
 
 	public:
 		void setmap(float map_width_, float map_height_);
 		void setstepsize(float step_size_);
+		void setgoalbias(float goal_bias_);
 		void setmaxiterations(int max_iterations_);
 		void setgoalradius(float goal_radius_);
 		void setrandompointsize(float randompoint_size_);
 		bool isGoal(Vec2i source_, Vec2i goal_); // check if the coordinate is at goal pos
 		bool isValid(Vec2i coordinates_, Vec2i closestvertex_); // check if the coordinate is valid
 		float euclidean_dis(Vec2i source_, Vec2i goal_); // calculate the euclidean distance from current to goal
-		Vec2i GenerateRandomPoint();
+		Vec2i GenerateRandomPoint(Vec2i goal_);
 		Vertex* getClosestVertex(std::set<Vertex*>& Vertices_, Vec2i randompoint_);
 		bool movetorandom(Vertex* closertvertex_, Vec2i randompoint_);
 		void findPath(Vec2i source_, Vec2i goal_);
